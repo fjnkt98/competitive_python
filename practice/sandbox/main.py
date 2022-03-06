774a015e9cb53e48f15e6dcb505f6f869e86f6c4
fjnkt98 <fjnkt98@gmail.com>
2022-03-06 13:16:46 +0900

    Test class implementation of binominal coefficient

--- practice/sandbox/main.py
+++ practice/sandbox/main.py
@@ -8,129 +8,42 @@ sys.setrecursionlimit(10000)
 input = sys.stdin.readline
 
 
-class EratosthenesSieve:
-    """
-    Class implementation of eratosthenes sieve
-    """
-
-    def __init__(self, N: int):
-        """
-        Constructor takes length of sieve
-        """
+class BinominalCoefficient:
+    def __init__(self, N: int, mod: int):
         self._N: int = N
-        self._is_prime: List[bool] = [True for i in range(N + 1)]
-        self._primes: List[int] = []
-        self._minimum_factor_of: List[int] = [-1 for i in range(N + 1)]
+        self._mod: int = mod
+        self._factorial: List[int] = [1 for i in range(N + 1)]
+        self._inverse_element: List[int] = [1 for i in range(N + 1)]
+        self._inverse_factorial: List[int] = [1 for i in range(N + 1)]
 
-        self._is_prime[0] = self._is_prime[1] = False
-        self._minimum_factor_of[1] = 1
+        for i in range(2, self._N + 1):
+            self._factorial[i] = self._factorial[i - 1] * i % self._mod
+            self._inverse_element[i] = (
+                self._mod
+                - self._inverse_element[self._mod % i] * (self._mod // i) % self._mod
+            )
+            self._inverse_factorial[i] = (
+                self._inverse_factorial[i - 1] * self._inverse_element[i] % self._mod
+            )
 
-        for i in range(2, N + 1):
-            if not self._is_prime[i]:
-                continue
+    def nCr(self, n: int, r: int) -> int:
+        if n > self._N or n < r or n < 0 or r < 0:
+            return 0
 
-            self._primes.append(i)
-            self._minimum_factor_of[i] = i
-
-            for j in range(2 * i, N + 1, i):
-                self._is_prime[j] = False
-
-                if self._minimum_factor_of[j] == -1:
-                    self._minimum_factor_of[j] = i
-
-    def factorize(self, N: int) -> List[Tuple[int, int]]:
-        """
-        Returns a list of pair of N prime factors and their exponents.
-        """
-        if N > self._N:
-            return []
-
-        result: List[Tuple[int, int]] = []
-        while N > 1:
-            p: int = self._minimum_factor_of[N]
-            exp: int = 0
-
-            while self._minimum_factor_of[N] == p:
-                N //= p
-                exp += 1
-
-            result.append((p, exp))
-
-        return result
-
-    def divisors(self, N: int) -> List[int]:
-        """
-        Returns a list of N's divisors.
-        """
-        if N > self._N:
-            return []
-
-        result: List[int] = [1]
-        factors: List[Tuple[int, int]] = self.factorize(N)
-
-        for p, exp in factors:
-            size: int = len(factors)
-            for i in range(size):
-                v: int = 1
-                for j in range(exp):
-                    v *= p
-                    result.append(result[i] * v)
-
-        return sorted(result)
-
-    def primes(self) -> List[int]:
-        """
-        Returns a list of prime numbers up to N.
-        """
-        return self._primes
-
-    def is_prime(self) -> List[bool]:
-        """
-        Returns a list of boolean indicating whether the number is prime or not up to N.
-        """
-        return self._is_prime
-
-
-def divisors(N: int) -> List[int]:
-    result: List[int] = []
-
-    i: int = 1
-    while i * i <= N:
-        if N % i == 0:
-            result.append(i)
-
-            if N // i != i:
-                result.append(N // i)
-        i += 1
-
-    return sorted(result)
-
-
-def factorize(N: int) -> List[Tuple[int, int]]:
-    result: List[Tuple[int, int]] = []
-
-    i: int = 2
-    while i * i <= N:
-        if N % i == 0:
-            exp: int = 0
-
-            while N % i == 0:
-                N //= i
-                exp += 1
-
-            result.append((i, exp))
-        i += 1
-
-    if N != 1:
-        result.append((N, 1))
-
-    return result
+        return (
+            self._factorial[n]
+            * (self._inverse_factorial[r] * self._inverse_factorial[n - r] % self._mod)
+            % self._mod
+        )
 
 
 def main():
-    es = EratosthenesSieve(30)
+    N = int(input())
+    k = int(input())
 
-    print(es.is_prime())
+    bc = BinominalCoefficient(N + k, 1000000007)
+
+    print(bc.nCr(N + k - 1, N - 1))
 
 
 if __name__ == "__main__":
