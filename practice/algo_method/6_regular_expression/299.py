456efe1cc6f2ac6ff0d0f217c0b53dc8d353855c
fjnkt98 <fjnkt98@gmail.com>
2022-03-27 00:25:08 +0900

    Daily study

---
+++ practice/algo_method/6_regular_expression/299.py
@@ -0,0 +1,19 @@
+from typing import List, Tuple
+import sys
+import collections
+import itertools
+import re
+
+
+sys.setrecursionlimit(1000000)
+input = sys.stdin.readline
+
+
+def main():
+    S: str = input().rstrip()
+    pattern: str = r"^a{1,5}b{10}c*$"
+    print("Yes" if re.search(pattern, S) else "No")
+
+
+if __name__ == "__main__":
+    main()
