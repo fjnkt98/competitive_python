83789b4df5c2d28c490804e1e700d2943bcea78c
fjnkt98 <fjnkt98@gmail.com>
2022-07-27 21:09:45 +0900

    Make index of dp table into right-open interval

--- practice/carefully_100/046.py
+++ practice/carefully_100/046.py
@@ -27,22 +27,22 @@ def main():
     R, C = map(list, zip(*[list(map(int, input().split())) for i in range(N)]))
 
     dp: List[List[int]] = [
-        [0 if i == j else 1 << 60 for j in range(N + 1)] for i in range(N + 1)
+        [0 if i + 1 == j else 1 << 60 for j in range(N + 2)] for i in range(N + 1)
     ]
 
     R = [0] + R
     C = [0] + C
 
     for i in range(1, N):
-        dp[i][i + 1] = R[i] * R[i + 1] * C[i + 1]
+        dp[i][i + 2] = R[i] * R[i + 1] * C[i + 1]
 
-    for l in range(2, N + 1):
-        for i in range(1, N - l + 1):
+    for l in range(3, N + 1):
+        for i in range(1, N + 2 - l):
             j: int = i + l
-            for k in range(i, j + 1):
-                dp[i][j] = min(dp[i][j], dp[i][k - 1] + dp[k][j] + R[i] * R[k] * C[j])
+            for k in range(i, j):
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j] + R[i] * R[k] * C[j - 1])
 
-    print(dp[1][N])
+    print(dp[1][N + 1])
 
 
 if __name__ == "__main__":
