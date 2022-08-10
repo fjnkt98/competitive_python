6cdbf10a74a732344373a0419cacd3043bcaac3c
fjnkt98 <fjnkt98@gmail.com>
2022-08-10 12:16:26 +0900

    update

--- contest/abc/263/d.py
+++ contest/abc/263/d.py
@@ -1,26 +1,16 @@
 from typing import (
     List,
-    Tuple,
-    Deque,
-    Set,
-    Dict,
     TypeVar,
     Callable,
     Generic,
-    Iterable,
     Iterator,
     Union,
 )
 import sys
-import collections
 import itertools
-import bisect
-import math
-
 
 sys.setrecursionlimit(1000000)
 input = sys.stdin.readline
-
 
 # Type of the element of segment tree
 S = TypeVar("S")
@@ -36,71 +26,101 @@ class SegmentTree(Generic[S]):
         _op (Callable[[S, S], S]): A function object representing the binary operator.
         _e (Callable[[], int]): A function object which returns identity element.
         _log (int): The logarithm of size of segment tree base 2.
-        _size (int): Size of the list that representing binary tree object.
+                    Also this represent the height of the tree.
+        _size (int): Number of the leaves of the tree
         _data (List[S]): A list of the entities representing segment tree.(1-indexed)
 
     """
 
     def __init__(self, op: Callable[[S, S], S], e: Callable[[], S], A: List[S]):
+        """Constructor
+
+        Args:
+            op (Callable[[S, S], S]): A function representing the binary operator.
+            e (Callable[[], S]): A function representing identity element.
+            A (List[S]): A list containing the initial values of leaves.
+        """
         self._N = len(A)
         self._op = op
         self._e = e
+        # Calculate tree size.
         self._log: int = (self._N - 1).bit_length()
         self._size: int = 1 << self._log
+        # Segment tree is represented by a list of length 2N
+        # because it's a full binary tree.
         self._data: List[S] = [self._e()] * (2 * self._size)
 
         # Initialize leaves with given list A.
         self._data[self._size : self._size + self._N] = A
 
-        # Update all nodes (not leaf).
+        # Update all non-leaf nodes.
         for i in range(self._size - 1, 0, -1):
             self._update(i)
 
-    def get(self, k: int) -> S:
-        """Get the value of the specified leaf.
+    def __setitem__(self, key: int, value: S) -> None:
+        """Point Update: Set the value into the specified leaf.
 
         Args:
-            k (int): The index of the leaf(0-indexed).
-
-        Returns:
-            S: Value of the specified leaf.
-
+            key (int): The index of the leaf (0-indexed).
+            value (S): The value to apply.
         """
 
-        return self._data[k + self._size]
-
-    def set(self, k: int, x: S) -> None:
-        """Set x into the specified leaf.
-
-        Args:
-            k (int): The index of the leaf(0-indexed).
-            x (S): The value to apply.
-
-        """
+        # Type check
+        if not isinstance(key, int):
+            raise TypeError
 
         # Move to the leaf.
-        k += self._size
+        key += self._size
 
         # Set the value of the leaf
-        self._data[k] = x
+        self._data[key] = value
 
         # Update value of the element from the leaf to the root.
         for i in range(1, self._log + 1):
-            self._update(k >> i)
+            self._update(key >> i)
+
+    def __getitem__(self, key: Union[int, slice]) -> S:
+        """Point Acquisition: Get the production of the specified point of interval.
+
+        Args:
+            key (int or slice): The index of the leaf, or interval (0-indexed).
+            if key is int, return the value of the leaf.
+            if key is slice, return the product of the interval.
+
+        Return:
+            S: The product.
+        """
+        # Type check
+        if isinstance(key, int):
+            # When key is int, return the value of the leaf.
+            return self._data[key + self._size]
+        elif isinstance(key, slice):
+            # Value check
+            l: int = 0 if key.start is None else key.start
+            r: int = self._N if key.stop is None else key.stop
+
+            if not (0 <= l < self._size and 0 <= r <= self._size):
+                raise IndexError
+
+            # WHen key is slice, return the value of the leaf
+
+            return self.prod(l, r)
+
+        else:
+            raise TypeError
 
     def prod(self, l: int, r: int) -> S:
         """Returns op(A[l], ..., A[r - 1]).
 
-        Returns the result of applying the binary operator to the interval [l, r).
+        Returns the product of the interval [l, r).
 
         Args:
             l (int): Left end of the given interval.
             r (int): Right end of the given interval. it doesn't include
                      the right end.
 
-        Returns:
-            S: The result.
-
+        Return:
+            S: The product.
         """
 
         # When invalid interval was given
@@ -122,12 +142,12 @@ class SegmentTree(Generic[S]):
             if l & 1:
                 # Calculate result.
                 left_result = self._op(left_result, self._data[l])
-                # Move to elder brother.
+                # Move to elder sibling.
                 l += 1
 
             # If r is right child
             if r & 1:
-                # Move to little brother.
+                # Move to little sibling.
                 r -= 1
                 # Calculate result.
                 right_result = self._op(right_result, self._data[r])
@@ -138,17 +158,6 @@ class SegmentTree(Generic[S]):
 
         # Return the result.
         return self._op(left_result, right_result)
-
-    def prod_all(self) -> S:
-        """Returns op(A[0], ..., A[N - 1]).
-
-        Return the result of applying the binary operator to all monoids.
-
-        Returns:
-            S: The operation result.
-
-        """
-        return self._data[1]
 
     def _update(self, k: int) -> None:
         """Update the element.
@@ -161,23 +170,26 @@ class SegmentTree(Generic[S]):
         """
         self._data[k] = self._op(self._data[2 * k], self._data[2 * k + 1])
 
+    def __len__(self) -> int:
+        """Return the size of tree."""
+        return 2 * self._size
+
+    def __iter__(self) -> Iterator[S]:
+        """Return the leaves iterator corresponding to A"""
+        for d in self._data[self._size : self._size + self._N]:
+            yield d
+
 
 def main():
     N, L, R = map(int, input().split())
     A: List[int] = list(map(int, input().split()))
 
-    S: int = sum(A)
-
     C: List[int] = [0] + list(itertools.accumulate(A))
+    D: List[int] = [C[y] - R * y for y in range(N + 1)]
 
-    seg = SegmentTree[int](
-        lambda a, b: min(a, b), lambda: 1 << 60, [C[i] - R * i for i in range(N + 1)]
-    )
-    answer: int = S
-    for x in range(N + 1):
-        answer = min(answer, R * N + L * x - C[x] + seg.prod(x, N + 1))
+    seg = SegmentTree[int](min, lambda: 1 << 60, D)
 
-    print(answer)
+    print(min([R * N + L * x - C[x] + seg[x : N + 1] for x in range(N + 1)]))
 
 
 if __name__ == "__main__":
