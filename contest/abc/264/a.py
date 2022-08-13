691a4cfe3394954f3de2e9d77a918e8c43b8eff3
fjnkt98 <fjnkt98@gmail.com>
2022-08-14 08:05:41 +0900

    daily study

---
+++ contest/abc/264/a.py
@@ -0,0 +1,31 @@
+from typing import (
+    List,
+    Tuple,
+    Deque,
+    Set,
+    Dict,
+    TypeVar,
+    Callable,
+    Generic,
+    Iterable,
+    Iterator,
+    Union,
+)
+import sys
+import collections
+import itertools
+import bisect
+import math
+
+
+sys.setrecursionlimit(1000000)
+input = sys.stdin.readline
+
+
+def main():
+    L, R = map(int, input().split())
+    print("atcoder"[L - 1 : R])
+
+
+if __name__ == "__main__":
+    main()
