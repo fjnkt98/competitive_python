284ac80dc7c871449def0631370cb8837e078f90
fjnkt98 <fjnkt98@gmail.com>
2022-10-02 16:32:01 +0900

    よさげ

--- practice/math_and_algorithm/071.py
+++ practice/math_and_algorithm/071.py
@@ -9,19 +9,20 @@ import numpy as np
 def main():
     N: int = int(input())
     M: np.ndarray = np.array(
-        [list(map(int, input().split())) for i in range(N)], dtype=np.int64
+        [list(map(int, input().split())) for i in range(N)], dtype=np.float64
     )
 
     n: int = N + 2
     m: int = N
 
-    A: np.ndarray = np.zeros((m, n))
+    A: np.ndarray = np.zeros((m, n), dtype=np.float64)
     A[:, : (n - m)] = M[:, : (n - m)]
     A[:, (n - m) :] = np.eye(m)
     b: np.ndarray = M[:, 2]
 
-    c: np.ndarray = np.array([0 for i in range(n)])
-    c[:2] = 1
+    c: np.ndarray = np.array([0 for i in range(n)], dtype=np.float64)
+    c[0] = 1
+    c[1] = 1
 
     basis_indices: List[int] = list(range(2, m + 2))
     non_basis_indices: List[int] = list(range(2))
@@ -30,21 +31,22 @@ def main():
     B: np.ndarray = A[:, basis_indices]
     cn: np.ndarray = c[non_basis_indices]
     cb: np.ndarray = c[basis_indices]
-
+    xb: np.ndarray = b.copy()
+    xn: np.ndarray = np.zeros(n - m)
     while True:
         # step. 2 被約費用の計算
         b_bar: np.ndarray = np.dot(np.linalg.inv(B), b)
-        c_bar: np.ndarray = cn - np.dot(np.dot(N, np.linalg.inv(B).T), cb)
+        c_bar: np.ndarray = cn - np.dot(N.T, np.dot(np.linalg.inv(B).T, cb))
 
         # step. 3
         # これ以上最適化できなくなったら終了
-        if (c_bar <= 0).all() == m:
-            xb: np.ndarray = b_bar
+        if (c_bar <= 0).all():
+            print("Stop")
             break
 
         # step.4
         # 被約費用が正かつ最小の添字kを取得する
-        k: int = np.argmin(c_bar > 0)
+        k = np.where(c_bar > 0)[0][0]
         N_bar: np.ndarray = np.dot(np.linalg.inv(B), N)
         # ベクトルaを取得する
         a_bar: np.ndarray = N_bar[:, k]
@@ -53,17 +55,28 @@ def main():
             print("Non bound")
             break
 
-        T: np.ndarray = np.where(a_bar > 0, a_bar / b_bar, np.inf)
+        T: np.ndarray = np.where(a_bar > 0, b_bar / a_bar, np.inf)
         theta: float = np.min(T)
         i: int = np.argmin(T)
 
         # step. 5
+        xn[k] = theta
+        xb = b_bar - theta * a_bar
         basis_indices[i], non_basis_indices[k] = non_basis_indices[k], basis_indices[i]
-        N[:, k], B[:, i] = B[:, i], N[:, k]
-        cn[k], cb[i] = cb[i], cn[k]
 
-        print(B)
-        print(N)
+        tmp: np.ndarray = N[:, k].copy()
+        N[:, k] = B[:, i]
+        B[:, i] = tmp
+
+        tmp = cn[k]
+        cn[k] = cb[i]
+        cb[i] = tmp
+
+    for i in range(m):
+        print(basis_indices[i], xb[i])
+
+    print(xb)
+    print(xn)
 
 
 if __name__ == "__main__":
